b74247338d36b10253dc41f7ea479820d8a8253c
longorion <longdang1985@gmail.com>
2024-10-25 22:53:50 +0700

    update new  25102024

--- template/js/jsmain.js
+++ template/js/jsmain.js
@@ -11,3 +11,18 @@ function changeContent(contentId) {
         activeContent.classList.remove('d-none');
     }
 }
+
+document.querySelector('.youtube-container').addEventListener('click', function() {
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/avd2y6ZvgG8?autoplay=1';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+    iframe.allowFullscreen = true;
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
+    this.innerHTML = '';
+    this.appendChild(iframe);
+});
